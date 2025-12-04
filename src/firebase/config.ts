851b8550b171848f4ac9c7d5a3e5e38d0f67f7f9
Monti Eltayeb <privatepple@gmail.com>
2025-12-04 22:27:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1455774830-cbb04",
+  "appId": "1:484573045990:web:8ef23260810d23bed48727",
+  "apiKey": "AIzaSyDvQ_S_tApEIkBSRBVt-d_Wdxe1W7ONCiw",
+  "authDomain": "studio-1455774830-cbb04.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "484573045990"
+};
