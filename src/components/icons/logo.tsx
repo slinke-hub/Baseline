bde4874aaa947265a2f3ca8451b1923a31b4c9e5
Monti Eltayeb <privatepple@gmail.com>
2025-12-09 23:21:58 +0000

    use this as a Logo

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     {...props}
-    viewBox="0 0 100 100"
+    viewBox="0 0 140 100"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
   >
@@ -9,57 +9,43 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     <defs>
       <linearGradient id="baseline-text-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
         <stop offset="0%" stopColor="#1E2A5D" />
+        <stop offset="60%" stopColor="#C41E3A" />
         <stop offset="100%" stopColor="#C41E3A" />
       </linearGradient>
     </defs>
     
-    <g transform="scale(0.95) translate(2, 2)">
-      {/* Main B shape */}
-      <path
-        d="M20,10 L50,10 L50,40 L30,40 C40,25 30,15 20,10 Z"
-        fill="#C41E3A"
-      />
-      <path
-        d="M20,45 L50,45 C75,45 75,70 50,80 L20,80 Z"
-        fill="#1E2A5D"
-      />
-      <path d="M20 10 L20 80" stroke="#1E2A5D" strokeWidth="6" fill="none"/>
-
-      {/* Red swoosh */}
-      <path 
-        d="M15,55 C40,35 70,45 90,40 C80,60 60,70 45,85 C55,80 65,75 70,65 C60,60 40,65 15,55 Z"
-        fill="#C41E3A"
-      />
-      {/* Blue swoosh */}
-      <path 
-        d="M15,50 C40,30 70,40 90,35 C80,55 60,65 45,80 C55,75 65,70 70,60 C60,55 40,60 15,50 Z"
-        fill="#1E2A5D"
-      />
+    <g transform="translate(10, 0)">
+      {/* Main B Shape */}
+      <path d="M20,10 L20,75 L50,75 C70,75 70,55 50,50 C70,45 70,25 50,15 L20,10 Z" fill="none" stroke="#1E2A5D" strokeWidth="6" />
+      <path d="M22,13 L48,16 V38 L22,40 Z" fill="#C41E3A"/>
+      <path d="M22,45 L48,48 C65,50 65,65 50,72 L22,72 Z" fill="#C41E3A" />
 
       {/* Backboard */}
-      <path d="M30,15 L45,15 L45,30 L30,30 Z" fill="#FFFFFF" />
-      <path d="M32,17 L43,17 L43,28 L32,28 Z" fill="none" stroke="#C41E3A" strokeWidth="1" />
-      <path d="M35,21 L40,21 L40,28 L35,28 Z" fill="none" stroke="#C41E3A" strokeWidth="1" />
+      <path d="M28,18 L42,19 L42,30 L28,31 Z" fill="#FFFFFF" />
+      <path d="M30,20 L40,21 V29 L30,28 Z" fill="none" stroke="#C41E3A" strokeWidth="0.8" />
+      <path d="M32,23 L38,24 V29 L32,27 Z" fill="none" stroke="#C41E3A" strokeWidth="0.8" />
       
-      {/* Hoop */}
-      <path d="M32,30 C35,35 40,35 43,30" fill="none" stroke="#C41E3A" strokeWidth="1.5" />
-      {/* Net */}
-      <path d="M33,31 L34,38 L35,31 M36,31 L37,38 L38,31 M39,31 L40,38 L41,31 M42,31 L41,38" stroke="#FFFFFF" strokeWidth="0.5" fill="none" />
-      <path d="M33,33 C37,35 40,35 42,33 M33.5,36 C37,38 40,38 41.5,36" stroke="#FFFFFF" strokeWidth="0.5" fill="none" />
-
+      {/* Hoop and Net */}
+      <path d="M28,32 C32,36 38,36 42,32" fill="none" stroke="#C41E3A" strokeWidth="1" />
+      <path d="M29,33 L30,40 L31,33 M32,33 L33,40 L34,33 M35,33 L36,40 L37,33 M38,33 L39,40 L40,33 M41,33 L40,40" stroke="#FFFFFF" strokeWidth="0.5" fill="none" />
+      <path d="M29,35 C35,37 38,37 41,35 M29.5,38 C35,40 38,40 40.5,38" stroke="#FFFFFF" strokeWidth="0.5" fill="none" />
+      
+      {/* Swooshes */}
+      <path d="M15,55 C40,35 70,45 90,40 C80,60 60,70 45,85 C55,80 65,75 70,65 C60,60 40,65 15,55 Z" fill="#1E2A5D" transform="translate(0, -2)" />
+      <path d="M15,50 C40,30 70,40 90,35 C80,55 60,65 45,80 C55,75 65,70 70,60 C60,55 40,60 15,50 Z" fill="#C41E3A" transform="translate(0, -2)" />
+      
       {/* Basketball */}
-      <g transform="translate(55, 55)">
+      <g transform="translate(60, 58)">
         <circle cx="0" cy="0" r="12" fill="#D95E0C" />
         <path d="M0,-12 A12,12 0 0,1 0,12" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
         <path d="M-12,0 A12,12 0 0,0 12,0" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
         <path d="M-9.5,-7.5 A10,10 0 0,1 9.5,7.5" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
         <path d="M-9.5,7.5 A10,10 0 0,0 9.5,-7.5" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
       </g>
-      
-      {/* Text BASELINE */}
-      <text x="5" y="95" fontFamily="Inter, sans-serif" fontSize="14" fontWeight="bold" fontStyle="italic" fill="url(#baseline-text-gradient)">
-        BASELINE
-      </text>
     </g>
+    
+    <text x="25" y="95" fontFamily="Inter, sans-serif" fontSize="18" fontWeight="bold" fontStyle="italic" fill="url(#baseline-text-gradient)">
+      BASELINE
+    </text>
   </svg>
 ));
