c0fd0b6caf4b2f2d09f91734a44a87b8decf7b5d
Monti Eltayeb <privatepple@gmail.com>
2025-12-09 23:28:45 +0000

    Use this as the App's logo

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,51 +1,67 @@
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    {...props}
-    viewBox="0 0 140 100"
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-  >
-    <title>Baseline Logo</title>
-    <defs>
-      <linearGradient id="baseline-text-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" stopColor="#1E2A5D" />
-        <stop offset="60%" stopColor="#C41E3A" />
-        <stop offset="100%" stopColor="#C41E3A" />
-      </linearGradient>
-    </defs>
-    
-    <g transform="translate(10, -5)">
-      {/* Main B Shape */}
-      <path d="M20,10 L20,75 L50,75 C70,75 70,55 50,50 C70,45 70,25 50,15 L20,10 Z" fill="none" stroke="#1E2A5D" strokeWidth="6" />
-      <path d="M22,13 L48,16 V38 L22,40 Z" fill="#C41E3A"/>
-      <path d="M22,45 L48,48 C65,50 65,65 50,72 L22,72 Z" fill="#C41E3A" />
+    <svg
+      {...props}
+      viewBox="0 0 160 120"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <title>Baseline Logo</title>
+      <defs>
+        <linearGradient id="baseline-text-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%" stopColor="white" />
+          <stop offset="70%" stopColor="white" />
+          <stop offset="100%" stopColor="#C41E3A" />
+        </linearGradient>
+        <filter id="glow">
+          <feGaussianBlur stdDeviation="1.5" result="coloredBlur" />
+          <feMerge>
+            <feMergeNode in="coloredBlur" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </defs>
+      
+      <g transform="translate(10, 0)">
+        {/* Main B Shape */}
+        <g>
+          {/* Red shadow swoosh */}
+          <path d="M 20 60 C 40 40, 80 50, 110 40 C 95 65, 70 80, 45 95 C 60 90, 75 80, 85 70 C 65 65, 40 70, 20 60 Z" fill="#C41E3A" transform="translate(3,3)" opacity="0.7"/>
 
-      {/* Backboard */}
-      <path d="M28,18 L42,19 L42,30 L28,31 Z" fill="#FFFFFF" />
-      <path d="M30,20 L40,21 V29 L30,28 Z" fill="none" stroke="#C41E3A" strokeWidth="0.8" />
-      <path d="M32,23 L38,24 V29 L32,27 Z" fill="none" stroke="#C41E3A" strokeWidth="0.8" />
-      
-      {/* Hoop and Net */}
-      <path d="M28,32 C32,36 38,36 42,32" fill="none" stroke="#C41E3A" strokeWidth="1" />
-      <path d="M29,33 L30,40 L31,33 M32,33 L33,40 L34,33 M35,33 L36,40 L37,33 M38,33 L39,40 L40,33 M41,33 L40,40" stroke="#FFFFFF" strokeWidth="0.5" fill="none" />
-      <path d="M29,35 C35,37 38,37 41,35 M29.5,38 C35,40 38,40 40.5,38" stroke="#FFFFFF" strokeWidth="0.5" fill="none" />
-      
-      {/* Swooshes */}
-      <path d="M15,55 C40,35 70,45 90,40 C80,60 60,70 45,85 C55,80 65,75 70,65 C60,60 40,65 15,55 Z" fill="#1E2A5D" transform="translate(0, -2)" />
-      <path d="M15,50 C40,30 70,40 90,35 C80,55 60,65 45,80 C55,75 65,70 70,60 C60,55 40,60 15,50 Z" fill="#C41E3A" transform="translate(0, -2)" />
-      
-      {/* Basketball */}
-      <g transform="translate(60, 58)">
-        <circle cx="0" cy="0" r="12" fill="#D95E0C" />
-        <path d="M0,-12 A12,12 0 0,1 0,12" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
-        <path d="M-12,0 A12,12 0 0,0 12,0" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
-        <path d="M-9.5,-7.5 A10,10 0 0,1 9.5,7.5" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
-        <path d="M-9.5,7.5 A10,10 0 0,0 9.5,-7.5" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
+          {/* Top part of B */}
+          <path d="M 20,10 L 60,10 C 85,10 85,35 60,40 L 20,40 V 10 Z" fill="#C41E3A" stroke="white" strokeWidth="3"/>
+          
+          {/* Bottom part of B */}
+          <path d="M 20,45 L 60,45 C 90,45 90,70 60,75 L 20,75 V 45 Z" fill="#C41E3A" stroke="white" strokeWidth="3"/>
+
+          {/* White swoosh */}
+          <path d="M 20 60 C 40 40, 80 50, 110 40 C 95 65, 70 80, 45 95 C 60 90, 75 80, 85 70 C 65 65, 40 70, 20 60 Z" fill="white"/>
+
+          {/* Court inside B */}
+          <path d="M 25,15 L 55,15 L 55,35 L 25,35 Z" fill="#1E2A5D" opacity="0.5" />
+          <path d="M 35,15 A 10 10 0 0 0 35 35" fill="none" stroke="white" strokeWidth="1" />
+          <path d="M 25,49 L 55,49 L 55,70 L 25,70 Z" fill="#1E2A5D" opacity="0.5" />
+          <path d="M 35,49 A 10 10 0 0 1 35 70" fill="none" stroke="white" strokeWidth="1" />
+
+          {/* Hoop and Net */}
+          <g transform="translate(32, 38)">
+            <path d="M 0,0 C 4,5 12,5 16,0" fill="none" stroke="#C41E3A" strokeWidth="1.2" />
+            <path d="M 1,1 L 2,8 L 3,1 M 4,1 L 5,8 L 6,1 M 7,1 L 8,8 L 9,1 M 10,1 L 11,8 L 12,1 M 13,1 L 14,8 L 15,1" stroke="white" strokeWidth="0.5" fill="none" />
+            <path d="M 1,3 C 8,4 12,4 15,3 M 1.5,6 C 8,7 12,7 14.5,6" stroke="white" strokeWidth="0.5" fill="none" />
+          </g>
+        </g>
+        
+        {/* Basketball */}
+        <g transform="translate(75, 60)">
+          <circle cx="0" cy="0" r="14" fill="#D95E0C" />
+          <path d="M0,-14 A14,14 0 0,1 0,14" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
+          <path d="M-14,0 A14,14 0 0,0 14,0" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
+          <path d="M-10.5,-9.5 A12,12 0 0,1 10.5,9.5" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
+          <path d="M-10.5,9.5 A12,12 0 0,0 10.5,-9.5" fill="none" stroke="#1E2A5D" strokeWidth="1"/>
+        </g>
       </g>
-    </g>
-    
-    <text x="25" y="95" fontFamily="Inter, sans-serif" fontSize="18" fontWeight="bold" fontStyle="italic" fill="url(#baseline-text-gradient)">
-      BASELINE
-    </text>
-  </svg>
-));
+      
+      <text x="30" y="105" fontFamily="Inter, sans-serif" fontSize="20" fontWeight="bold" fontStyle="italic" fill="url(#baseline-text-gradient)" filter="url(#glow)">
+        BASEL<tspan dy="-3" fontSize="14">'</tspan><tspan dy="3">INE</tspan>
+      </text>
+    </svg>
+  );
