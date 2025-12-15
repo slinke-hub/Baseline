bde9b69aeabd9047d63c22d875188472240c92dd
Monti Eltayeb <privatepple@gmail.com>
2025-12-15 06:49:26 +0000

    Error: Directory 'out' for Hosting does not exist.
    I get this when I ru

--- next.config.ts
+++ next.config.ts
@@ -24,8 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
