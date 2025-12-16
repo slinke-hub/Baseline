dadd9cefe3c945f0382b80e9901c0abdc7a7d427
Monti Eltayeb <privatepple@gmail.com>
2025-12-16 22:13:40 +0000

    submiting new court always fail, please fix that

--- src/app/(app)/locations/page.tsx
+++ src/app/(app)/locations/page.tsx
@@ -199,7 +199,7 @@ export default function LocationsPage() {
                                                 </div>
                                             </div>
                                         )}
-                                        <Input type="file" accept="image/*" ref={fileInputRef} onChange={handleFileChange} required />
+                                        <Input type="file" accept="image/*" ref={fileInputRef} onChange={handleFileChange} />
                                     </div>
                                 </div>
                                 <DialogFooter>
@@ -275,5 +275,3 @@ export default function LocationsPage() {
         </div>
     );
 }
-
-    
