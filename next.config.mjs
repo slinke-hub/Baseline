5f82381aef183c0a82a2e5923db6217808bb15d3
Monti Eltayeb <privatepple@gmail.com>
2025-12-20 23:17:39 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.mjs
@@ -0,0 +1,41 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
