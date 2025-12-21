ff95c989686afec6b19987b671f91dbad4b8773c
Monti Eltayeb <249641538+slinke-hub@users.noreply.github.com>
2025-12-21 20:52:57 +0000

    fit the icon to the sidebar header

--- src/components/layout/admin-sidebar.tsx
+++ src/components/layout/admin-sidebar.tsx
@@ -37,7 +37,7 @@ export function AdminSidebar() {
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-16 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex h-16 items-center justify-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/admin" className="flex items-center gap-2 font-semibold">
             <Logo width={180} height={52}/>
           </Link>
