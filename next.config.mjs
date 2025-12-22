4b995cd10625b8ac48ad6c7bc194ae5a80e73a95
Monti Eltayeb <249641538+slinke-hub@users.noreply.github.com>
2025-12-22 10:28:55 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default withPWA(nextConfig);
+import withPWA from '@ducanh2912/next-pwa';
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+            },
+        ],
+    },
+};
+
+const pwaConfig = withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+});
+
+export default pwaConfig(nextConfig);
