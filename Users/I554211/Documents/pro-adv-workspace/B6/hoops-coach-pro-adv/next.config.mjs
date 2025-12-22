7708462fe0381d23b7d4ee42cb1600a3d1430306
Monti Eltayeb <249641538+slinke-hub@users.noreply.github.com>
2025-12-22 11:54:46 +0000

    Try fixing this error: `Server Error: Error: Invalid Firebase app option

---
+++ Users/I554211/Documents/pro-adv-workspace/B6/hoops-coach-pro-adv/next.config.mjs
@@ -0,0 +1,34 @@
+
+import withPWA from '@ducanh2912/next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
+    ],
+  },
+  env: {
+    FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT,
+  },
+};
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+export default pwaConfig(nextConfig);
