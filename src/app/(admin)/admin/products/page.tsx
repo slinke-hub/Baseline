f93fa279090e52dd024ecea958cd6f5ff921b9c4
Monti Eltayeb <249641538+slinke-hub@users.noreply.github.com>
2025-12-25 05:19:41 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: AlertDi

--- src/app/(admin)/admin/products/page.tsx
+++ src/app/(admin)/admin/products/page.tsx
@@ -26,6 +26,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import {
   Dialog,
@@ -310,3 +311,5 @@ export default function AdminProductsPage() {
         </Card>
     )
 }
+
+    
